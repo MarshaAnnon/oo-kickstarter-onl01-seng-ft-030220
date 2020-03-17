2bfd20bfa4deca7883ea0eba8bff8aaf7dbd3984
Marsha Annon <marshaannon@gmail.com>
2020-03-17 23:34:29 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -4,12 +4,13 @@ class Project
 
   attr_accessor :backer
   attr_reader :title
+  @backers =[]
 
 
   def initialize(title)
     @title = title
     @backer = backer
-    @backers =[]
+    @backers = backers
 
   end
 
