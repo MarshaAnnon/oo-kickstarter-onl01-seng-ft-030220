8f32d1f37ba71579c61a1ddffb97196be9fab2ac
Marsha Annon <marshaannon@gmail.com>
2020-03-17 23:35:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,12 +2,14 @@ require "pry"
 
 class Backer
 
-attr_accessor :backed_projects
+attr_accessor :backed_projects, backers
 attr_reader :name
 @@backed_projects = []
+
   def initialize(name)
     @name = name
     @backed_projects = []
+    @backers = backers
   end
 
 
