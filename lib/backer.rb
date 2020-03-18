f3ef6ff75e2ff345df35a41d4532dd6573eeb6a9
Marsha Annon <marshaannon@gmail.com>
2020-03-18 01:13:53 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,7 +12,7 @@ attr_reader :name
   end
 
   def back_project(project)
-    @backed_projects << project
+    @backed_projects << Project.new
     project.add_backer(self) unless project.backers.include?(self)
   end
 
